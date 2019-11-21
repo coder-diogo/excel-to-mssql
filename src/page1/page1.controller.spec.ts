6bed01563fd0b16ab777eae902fee7949da0acef
Diogo Adão <dev_diogo.adao@hotmail.com>
2019-11-21 12:22:26 +0000

    cleaned page1 Tests

--- src/page1/page1.controller.spec.ts
+++ src/page1/page1.controller.spec.ts
@@ -3,6 +3,7 @@ import createMockInstance from 'jest-create-mock-instance';
 import { Test, TestingModule } from '@nestjs/testing';
 import { Page1Service } from './page1.service';
 import { Page1Repository } from './page1.repository';
+import { Page1Controller } from './page1.controller';
 
 const mockPage1Repository = () => ({
   create: jest.fn(),
@@ -10,8 +11,8 @@ const mockPage1Repository = () => ({
   merge: jest.fn(),
 });
 
-describe('Page1 Service', () => {
-  let page1Service: Page1Service;
+describe('Page1 Controller', () => {
+  let page1Controller: Page1Controller;
   let page1Repository: any;
   let mockPage1: jest.Mocked<Page1>;
   const page1Dto: any = {
@@ -34,29 +35,32 @@ describe('Page1 Service', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      controllers: [Page1Controller],
       providers: [Page1Service,
         { provide: Page1Repository, useFactory: mockPage1Repository },
       ],
     }).compile();
-
     page1Repository = module.get<Page1Repository>(Page1Repository);
-    page1Service = module.get<Page1Service>(Page1Service);
+    page1Controller = module.get<Page1Controller>(Page1Controller);
+
     mockPage1 = createMockInstance(Page1);
   });
 
-  describe('Page1 Repository', () => {
-    it('should be created', async () => {
-      page1Repository.findOne.mockResolvedValue();
-      mockPage1.save.mockResolvedValue(page1Dto);
-      page1Repository.create.mockReturnValue(mockPage1);
-      expect(await page1Service.createOrUpdate(page1Dto)).toBe(page1Dto);
-    });
+  describe('Page1 Service', () => {
+    describe('Page1 Repository', () => {
+      it('should be created', async () => {
+        page1Repository.findOne.mockResolvedValue();
+        mockPage1.save.mockResolvedValue(page1Dto);
+        page1Repository.create.mockReturnValue(mockPage1);
+        expect(await page1Controller.createOrUpdate(page1Dto)).toBe(page1Dto);
+      });
 
-    it('should be Updated', async () => {
-      page1Repository.findOne.mockResolvedValue(page1Dto);
-      mockPage1.save.mockResolvedValue(page1Dto);
-      page1Repository.merge.mockReturnValue(mockPage1);
-      expect(await page1Service.createOrUpdate(page1Dto)).toBe(page1Dto);
+      it('should be Updated', async () => {
+        page1Repository.findOne.mockResolvedValue(page1Dto);
+        mockPage1.save.mockResolvedValue(page1Dto);
+        page1Repository.merge.mockReturnValue(mockPage1);
+        expect(await page1Controller.createOrUpdate(page1Dto)).toBe(page1Dto);
+      });
     });
   });
 });
